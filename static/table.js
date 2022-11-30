aaec04b1659de6e4c68fa4c52fba170eb9a1a4e1
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2022-11-30 17:05:03 -0500

    working on sorting function for the table

--- static/table.js
+++ static/table.js
@@ -1,4 +1,10 @@
 function sortTable(table, col = "", asc = true) {
+    const dirModifier = asc ? 1 : -1;
     const body = table.tBodies[0];
     const rows = Array.from(body.querySelectorAll("tr"))
+
+    // now sort them
+    var sortedRows = rows.sort((a, b) => {
+
+    })
 }
