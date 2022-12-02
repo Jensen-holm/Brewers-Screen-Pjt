bb58b5e745e6dec94dfea558940770a31ac91b8a
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2022-12-02 18:47:03 -0500

    made the pitchresult column and cleaned up the graph code

--- data/clean.py
+++ data/clean.py
@@ -1,10 +1,23 @@
 import pandas as pd
 
-df: pd.DataFrame = pd.read_csv("20220423-Olsen-1.csv")
-
 
 def main():
-    return
+    """
+    goal is to make a new column titled PitchResult
+    which is the same as TaggedHitType except replacing
+    'undefined' values with the result from PitchCall
+    only had to run this function one time
+    :return saved the new csv with the added column discussed above:
+    """
+    df: pd.DataFrame = pd.read_csv("20220423-Olsen-1.csv")
+
+    for i in range(len(df["TaggedHitType"])):
+        if df.at[i, "TaggedHitType"] != "Undefined":
+            df.at[i, "PitchResult"]: str = df.at[i, "TaggedHitType"]
+        else:
+            df.at[i, "PitchResult"]: str = df.at[i, "PitchCall"]
+
+    df.to_csv("20220423-Olsen-1.csv")
 
 
 if __name__ == "__main__":
--- views/vis.py
+++ views/vis.py
@@ -10,12 +10,6 @@ sns.set()
 matplotlib.use("svg")
 
 
-# COLOR = "F5F5DCFF"
-# matplotlib.rcParams['axes.labelcolor'] = COLOR
-# matplotlib.rcParams['xtick.color'] = COLOR
-# matplotlib.rcParams['ytick.color'] = COLOR
-
-
 def save_plot(fig) -> None:
     fig.savefig(path_2_plots + "player_result_plot.svg")
 
@@ -23,12 +17,21 @@ def save_plot(fig) -> None:
 def plot(player_df, player_pos: str) -> None:
     fig, ax = plt.subplots(1, 3)
 
-    # fig.patch.set_facecolor("#555555")
     fig.set_figheight(8)
     fig.set_figwidth(24)
 
-    k_zone(player_df, ax=ax[0], title="Strike Zone w/ Pitch Type")
-    k_zone(player_df, ax=ax[1], color_by="TaggedHitType", title="Strike Zone w/ Hit Type")
+    k_zone(
+        player_df,
+        ax=ax[0],
+        title="Strike Zone w/ Pitch Type"
+    )
+
+    k_zone(
+        player_df,
+        ax=ax[1],
+        color_by="PitchResult",
+        title="Strike Zone w/ Result Type"
+    )
 
     # if it's a pitcher
     if player_pos == "Pitcher":
