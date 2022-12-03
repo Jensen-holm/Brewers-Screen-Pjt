ca2093ad2358a997196789f4481457aea5e61d51
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2022-12-03 18:03:28 -0500

    working on uploading other trackman files not going super well, might need to rethink it and listen for csv upload requests in index()

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import pandas as pd
+from data.fp import path_to_data
 from flask import Flask, render_template, request
-from views.table import tbl
+from views.table import TrackmanData
 from routes.default import default
 from routes.result import result
 import os
@@ -10,18 +12,22 @@ app = Flask(
     static_folder="static"
 )
 
+app.config["UPLOAD_FOLDER"] = path_to_data.split("/")[:-1]
+
 
 @app.route("/pitchers", methods=["GET"])
 @app.route("/hitters", methods=["GET"])
 @app.route("/hitter_result", methods=["GET", "POST"])
 @app.route("/pitcher_result", methods=["GET", "POST"])
-def index():
+def index(tbl: TrackmanData = TrackmanData()):
     """
     Main function for the index.html template
     Takes no arguments, most of the logic handling
     requests is inside the routes' module
     :return index.html file with data depending on user:
     """
+
+    tbl.read()
 
     def check_page(path: request.path):
         return "Batter" if "hitter" in path else "Pitcher"
@@ -55,16 +61,45 @@ def index():
     )
 
 
-# home screen
-@app.route("/", methods=["GET"])
+ALLOWED_EXTENSIONS: set[str] = {
+    "csv"
+}
+
+
+def allowed_file(file):
+    return True if file.split(".")[-1] in ALLOWED_EXTENSIONS else False
+
+
+@app.route("/", methods=["GET", "POST"])
+@app.route("/pitchers_custom", methods=["GET", "POST"])
+@app.route("/hitters_custom", methods=["GET", "POST"])
 def home():
     """
     Renders home.html file, serves as the home page with a data
     dictionary as well as a few notes
     :return home.html:
     """
+    if request.method == "POST":
+        if "file" not in request.files:
+            return render_template(
+                "home.html",
+                success="No file found in request"
+            )
+        if not allowed_file(request.form["trackman_csv"]):
+            return render_template(
+                "home.html",
+                success="Invalid file type"
+            )
+
+        # if there were no problems, insert the csv into the analysis pages
+        # will need to save it into the data folder
+        return render_template(
+            "index.html",
+        )
+
     return render_template(
-        "home.html"
+        "home.html",
+        success="team photos will only be shown for default csv"
     )
 
 
