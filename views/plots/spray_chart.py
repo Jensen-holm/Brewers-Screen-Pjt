d82dc54825c3ef990212f3f34194d5bb2f151a71
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2022-12-04 14:51:37 -0500

    figured out spray chart

--- views/plots/spray_chart.py
+++ views/plots/spray_chart.py
@@ -1,18 +1,41 @@
+import os
 import pandas as pd
 import seaborn as sns
 import math
+import matplotlib.pyplot as plt
+from matplotlib.offsetbox import AnnotationBbox, OffsetImage
+from PIL import Image
 
 
 def spray_chart(player_df: pd.DataFrame, ax):
-    # calculate point on circumference of circle with radius of distance
-    dist = player_df["Distance"].dropna().to_numpy()
-    angle = player_df["Bearing"].dropna().to_numpy()
+    dist = player_df["Distance"].to_numpy()
+    angle = player_df["Bearing"].to_numpy()
 
-    x = [math.fabs(d * math.cos(math.radians(angle[i]))) for i, d in enumerate(dist)]
-    y = [math.fabs(d * math.sin(math.radians(angle[i]))) for i, d in enumerate(dist)]
-    print(x, y)
+    player_df["hc_x"] = [math.fabs(d * math.cos(angle[i])) for i, d in enumerate(dist)]
+    player_df["hc_y"] = [math.fabs(d * math.sin(angle[i])) for i, d in enumerate(dist)]
+    player_df["events"] = player_df["PlayResult"]
+
+    aff = Image.open(os.getcwd() + "/views/plots/aff.png") \
+        .rotate(90 + 180).resize((1500, 1500))
+    ax.imshow(aff, extent=(-265, 600, -310, 600))
 
     sns.scatterplot(
-        x=x,
-        y=y
+        data=player_df,
+        x="hc_x",
+        y="hc_y",
+        hue="PlayResult",
+        s=100
+    )
+    ax.set_ylim(-50, 500)
+    ax.set_xlim(-50, 500)
+
+    # plot the baseball diamond
+    ax.add_patch(
+        plt.Rectangle(
+            (0, 0),
+            90,
+            90,
+            fc="none",
+            ec="brown"
+        )
     )
